3d155c2aa78365d0481a2a9599fff56ba5757166
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2023-04-02 13:21:29 -0400

    Delete old rules when total decreases

--- college/src/background.js
+++ college/src/background.js
@@ -10,31 +10,31 @@ chrome.browsingData.remove(
 
 const blockUrls = [
   'collegeconfidential.com',
-  'www.reddit.com/r/ApplyingToCollege/',
+  // 'www.reddit.com/r/ApplyingToCollege/',
   'linkedin.com',
 ];
+
+const newRules = [];
 
 blockUrls.forEach((domain, index) => {
   const id = index + 3;
 
-  chrome.declarativeNetRequest.updateDynamicRules({
-    addRules: [
-      {
-        id: id,
-        priority: 1,
-        action: {
-          type: 'redirect',
-          redirect: {
-            url: 'https://rutgers.edu/',
-          },
-        },
-        condition: {
-          isUrlFilterCaseSensitive: false,
-          urlFilter: domain,
-          resourceTypes: ['main_frame'],
-        },
+  newRules.push({
+    id: id,
+    priority: 1,
+    action: {
+      type: 'redirect',
+      redirect: {
+        url: 'https://rutgers.edu/',
       },
-    ],
-    removeRuleIds: [id],
+    },
+    condition: { urlFilter: domain, resourceTypes: ['main_frame'] },
+  });
+});
+chrome.declarativeNetRequest.getDynamicRules((previousRules) => {
+  const previousRuleIds = previousRules.map((rule) => rule.id);
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: previousRuleIds,
+    addRules: newRules,
   });
 });
