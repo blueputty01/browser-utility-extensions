06398dbfd19d18387a8fa23622a55de60283c680
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-10-06 15:27:10 -0400

    feat(canvas) decrease specificity of iframe heuristic

--- canvas/content.js
+++ canvas/content.js
@@ -1,9 +1,7 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  var iframe =
-    document.getElementsByClassName('ef-file-preview-frame')[0] ||
-    Array.from(document.getElementsByTagName('iframe')).find(function (x) {
-      return !x.id && !x.className;
-    });
+  var iframe = Array.from(document.getElementsByTagName('iframe')).find(
+    (x) => x.allowFullscreen
+  );
   if (!iframe) {
     return alert('Please open a file before clicking the extension');
   }
