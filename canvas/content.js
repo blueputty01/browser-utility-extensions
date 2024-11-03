614965fc449e12b7e50eaf3a14c319c08f84130c
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-11-03 17:14:22 -0500

    feat(canvas) remove content-disposition header

--- canvas/content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  var iframe = Array.from(document.getElementsByTagName('iframe')).find(
-    (x) => x.allowFullscreen
-  );
-  if (!iframe) {
-    return alert('Please open a file before clicking the extension');
-  }
-  window.open(iframe.src);
-});
